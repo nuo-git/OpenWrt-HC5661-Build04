c468237d2652523546eb2c6df18fb972e430fb89
nuo-git <75625708+nuo-git@users.noreply.github.com>
2020-12-11 01:25:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,9 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
+
+
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
